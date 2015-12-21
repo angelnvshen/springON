b4e198f33cc7ae844351965b5b51179ba6370a94
calvin <530678742@qq.com>
2015-12-21 15:57:47 +0800

    springON

---
+++ src/main/java/mvc/controller/HelloWorldController.java
@@ -0,0 +1,28 @@
+package mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Michael on 2015/12/21.
+ */
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/helloWorld")
+    public String helloWorld(Model model){
+        model.addAttribute("message", "Hello World");
+        return "helloWorld";
+    }
+
+    @RequestMapping(value = "/owners/{ownerId}",method = RequestMethod.GET  )
+//    public String findOwner(@PathVariable String ownerId, Model model){
+    public String findOwner(@PathVariable("ownerId") String theOnwer, Model model){
+        // Owner owner = ownerService.findOwner(ownerId);
+        // model.addAttribute("owner", owner);
+        return "displayOnwer";
+    }
+}
