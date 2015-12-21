fb10f0387e49f957f10220649c64c2eb706270bb
calvin <530678742@qq.com>
2015-12-21 17:32:08 +0800

    add some files

--- src/main/java/mvc/controller/HelloWorldController.java
+++ src/main/java/mvc/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Michael on 2015/12/21.
@@ -12,17 +13,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloWorldController {
 
+    @ResponseBody
     @RequestMapping("/helloWorld")
     public String helloWorld(Model model){
         model.addAttribute("message", "Hello World");
         return "helloWorld";
     }
 
+    @ResponseBody
     @RequestMapping(value = "/owners/{ownerId}",method = RequestMethod.GET  )
 //    public String findOwner(@PathVariable String ownerId, Model model){
     public String findOwner(@PathVariable("ownerId") String theOnwer, Model model){
         // Owner owner = ownerService.findOwner(ownerId);
         // model.addAttribute("owner", owner);
-        return "displayOnwer";
+        return "displayOnwer:"+ " - "+theOnwer ;
     }
 }
